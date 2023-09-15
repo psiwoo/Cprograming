6c7482a71a461df16ddea221bd80f78cee870597
psiwoo <126779239+psiwoo@users.noreply.github.com>
2023-09-15 18:54:40 +0900

    Create hw1.c

---
+++ hw1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+	int num1 = 0, num2 = 0;
+	printf("Input two integers: ");
+	scanf("%d %d", &num1, &num2);
+	int result1 = num1 & num2;
+	int result2 = num1 | num2;
+	int result3 = num1 ^ num2;
+	printf("%d&%d %d\n", num1, num2, result1);
+	printf("%d|%d %d \n", num1, num2, result2);
+	printf("%d^%d %d\n", num1, num2, result3);
+	return 0;
+}
